1d3229a370ec74a6cd20187cebd63d6a6485ebee
sqrtofsaturn <sqrtofsaturn@gmail.com>
2017-09-05 17:47:18 -0700

    Auto stop

--- go.js
+++ go.js
@@ -3,5 +3,9 @@
 const Robot = require('./lib/Robot')
 
 const robot = new Robot()
+const interval = setInterval(robot.forward, 100)
 
-setInterval(robot.forward, 100)
+process.on('SIGINT', () => {
+  clearInterval(interval)
+  robot.stop()
+})
