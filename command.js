cf5a61dc37628d4df0611fa25ab8b0f8b13cbc44
sqrtofsaturn <sqrtofsaturn@gmail.com>
2017-09-06 17:10:06 -0700

    Support arrow keys

---
+++ command.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+require('readline').emitKeypressEvents(process.stdin)
+const Robot = require('./lib/Robot')
+
+class Command {
+  constructor() {
+    process.stdin.setRawMode(true)
+    this.robot = new Robot()
+  }
+
+  run() {
+    console.log('press "escape" to quit')
+    process.stdin.on('keypress', (ignored, { name }) => {
+      if (name === 'up') this.robot.forward()
+      if (name === 'left') this.robot.left()
+      if (name === 'right') this.robot.right()
+      if (name === 'down') this.robot.stop()
+
+      if (name === 'escape') this.exit()
+    })
+  }
+
+  exit() {
+    this.robot.stop()
+    process.exit(0)
+  }
+}
+
+new Command().run()
